6c66c006015a018b66a33a46ee0dbf2624749474
vishalbinil <168182806+vishalbinil@users.noreply.github.com>
2025-02-17 23:28:58 +0530

    Update users.service.ts

--- users.service.ts
+++ users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from './user.schema'; // ✅ Correct import
+import { User, UserDocument } from './user.schema'; 
 
 @Injectable()
 export class UsersService {
