2430c87b4b779dd9bad73810bd6baf5e4e0528f4
vishalbinil <168182806+vishalbinil@users.noreply.github.com>
2025-02-17 23:25:19 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module'; 
+import { AppController } from './app.controller'; 
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/nest-auth'),
-    AuthModule, // Importing the AuthModule which contains AuthController and AuthService
+    AuthModule, 
+	UsersModule,
+	
   ],
+   controllers: [AppController]
 })
 export class AppModule {}
