c1580c833bf4733457d8cda1d1ef085fc739e6e9
vishalbinil <168182806+vishalbinil@users.noreply.github.com>
2025-02-17 23:28:14 +0530

    Update users.module.ts

--- users.module.ts
+++ users.module.ts
@@ -4,6 +4,7 @@ import { User, UserSchema } from './user.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
-  exports: [MongooseModule], // ✅ Ensure MongooseModule is exported
+  exports: [MongooseModule],
 })
 export class UsersModule {}
+
